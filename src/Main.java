f07274c3a6295a12eacc55a904c9686e2d8d9020
Peter Gutkovich <peterg94@nycstudents.net>
2024-09-19 13:00:03 -0400

    finished U1L5, prob gonna make a folder for each unit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World! Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ENTER 5 DIGIT INTEGER: ");
+
+        int num = scanner.nextInt();
+        int digit;
+
+        String incremented = "";
+
+        for (int i = 0; i < 5; i++) {
+            digit = num % 10;
+            num /= 10;
+
+            digit += 1;
+            digit %= 10;
+
+            incremented = Integer.toString(digit) + incremented;
+        }
+
+        System.out.println(incremented);
     }
 }
